9486378221b5d3713aa9977f2c2b238f5b747c59
Leandri Junior <scorpionswagner@gmail.com>
2023-07-02 01:22:29 -0300

    feat(Task/TodoUser): add new config to delete with status true/false

--- src/main/java/com/wagner/todolist/repositories/TaskRepository.java
+++ src/main/java/com/wagner/todolist/repositories/TaskRepository.java
@@ -2,11 +2,20 @@ package com.wagner.todolist.repositories;
 
 import com.wagner.todolist.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByUser_Id(Long Id);
+
+    List<Task> findByUser_Id(Long id);
+
+    @Modifying
+    @Query("UPDATE Task t SET t.status = true WHERE t.id = :id")
+    void deleteStatus(Long id);
+
 }
--- src/main/java/com/wagner/todolist/repositories/UserRepository.java
+++ src/main/java/com/wagner/todolist/repositories/UserRepository.java
@@ -2,9 +2,14 @@ package com.wagner.todolist.repositories;
 
 import com.wagner.todolist.models.TodoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<TodoUser, Long> {
+    @Modifying
+    @Query("UPDATE TodoUser u SET u.status = true WHERE u.id = :id")
+    void deleteStatus(Long id);
 
 }
