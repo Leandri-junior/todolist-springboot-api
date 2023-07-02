829c6b2690a9ddb13f9df7daa722902f73ba1fd7
Leandri Junior <scorpionswagner@gmail.com>
2023-07-02 00:10:22 -0300

    feat(TaskRepository):created TaskRepository

---
+++ src/main/java/com/wagner/todolist/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.wagner.todolist.repositories;
+
+import com.wagner.todolist.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
