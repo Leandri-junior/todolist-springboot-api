55c9f1385479ee05406b083a2b887eaef2dd3280
Leandri Junior <scorpionswagner@gmail.com>
2023-07-04 15:59:49 -0300

    fix(UserController): created get request with id

--- src/main/java/com/wagner/todolist/controllers/UserController.java
+++ src/main/java/com/wagner/todolist/controllers/UserController.java
@@ -1,4 +1,27 @@
 package com.wagner.todolist.controllers;
 
+import com.wagner.todolist.models.TodoUser;
+import com.wagner.todolist.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Validated
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoUser> findById(@PathVariable Long id){
+        TodoUser userObj = this.userService.findById(id);
+        return ResponseEntity.ok().body(userObj);
+    }
+
+
 }
